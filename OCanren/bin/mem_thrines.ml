2995ca937f685a2c20f818077cbb007180efd9e8
Artem Starikov <contact@snejugal.ru>
2025-02-26 20:53:51 +0300

    Make OCanren benchmarks a bit more precise (?)

--- OCanren/bin/mem_thrines.ml
+++ OCanren/bin/mem_thrines.ml
@@ -1,11 +1,12 @@
 open Benchmark
 open Scheme_interpret
 
-let { Gc.top_heap_words ; _ } = Gc.stat ();;
+let () = Gc.compact();;
+let { Gc.heap_words ; _ } = Gc.stat ();;
 let () =
     OCanren.(run qrs) thrineso (fun rr _ _ -> rr#reify OCanren.Reifier.id)
       |> OCanren.Stream.take ~n:2
       |> ignore;;
 let new_stat = Gc.stat ();;
-let heap_words = new_stat.Gc.top_heap_words - top_heap_words;;
-let () = Printf.printf "Peak allocated in major heap: %d B\n" (heap_words * 8);;
+let peak_allocated = new_stat.Gc.top_heap_words - heap_words;;
+let () = Printf.printf "Peak allocated in major heap: %d B\n" (peak_allocated * 8);;
